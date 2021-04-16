76b28987545196da2872fa1575b076c3b5714021
fedefaganelliVM <fedefaganelli.coc@gmail.com>
2021-04-15 21:22:02 -0300

    primer commit

---
+++ Debug/src/HelloWorldFede.d
@@ -0,0 +1,1 @@
+src/HelloWorldFede.o: ../src/HelloWorldFede.cpp
---
+++ src/HelloWorldFede.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorldFede.cpp
+// Author      : fedefaga
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World eeeee!!!" << endl; // prints !!!Hello World eeeee!!!
+	return 0;
+}
